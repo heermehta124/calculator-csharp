63398555daf6fe73d0b5f35aedbf9cd4ed604dd7
Heer Mehta <heermehtaa12@gmail.com>
2025-04-26 00:10:18 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // Initialize button positions and sizes
+            this.button1.Location = new System.Drawing.Point(10, 10);
+            this.button1.Size = new System.Drawing.Size(80, 80);
+            this.button2.Location = new System.Drawing.Point(100, 10);
+            this.button2.Size = new System.Drawing.Size(80, 80);
+            this.button3.Location = new System.Drawing.Point(190, 10);
+            this.button3.Size = new System.Drawing.Size(80, 80);
+            this.button4.Location = new System.Drawing.Point(10, 100);
+            this.button4.Size = new System.Drawing.Size(80, 80);
+            this.button5.Location = new System.Drawing.Point(100, 100);
+            this.button5.Size = new System.Drawing.Size(80, 80);
+            this.button6.Location = new System.Drawing.Point(190, 100);
+            this.button6.Size = new System.Drawing.Size(80, 80);
+            this.button7.Location = new System.Drawing.Point(10, 190);
+            this.button7.Size = new System.Drawing.Size(80, 80);
+            this.button8.Location = new System.Drawing.Point(100, 190);
+            this.button8.Size = new System.Drawing.Size(80, 80);
+            this.button9.Location = new System.Drawing.Point(190, 190);
+            this.button9.Size = new System.Drawing.Size(80, 80);
+
+            // Add buttons to the form
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+
+            // Form properties
+            this.ClientSize = new System.Drawing.Size(284, 284);
+            this.Name = "Form1";
+            this.Text = "TicTacToe_064";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private Boolean isPlayerXTurn = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Click += Button_Click;
+            button2.Click += Button_Click;
+            button3.Click += Button_Click;
+            button4.Click += Button_Click;
+            button5.Click += Button_Click;
+            button6.Click += Button_Click;
+            button7.Click += Button_Click;
+            button8.Click += Button_Click;
+            button9.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (isPlayerXTurn)
+            {
+                button.Text = "X";
+            }
+            else
+            {
+                button.Text = "O";
+            }
+
+            button.Enabled = false;
+            CheckWinCondition();
+            isPlayerXTurn = !isPlayerXTurn;
+        }
+
+        private void CheckWinCondition()
+        {
+            string[][] winningCombinations = new string[][]
+            {
+        new string[] { button1.Text, button2.Text, button3.Text },
+        new string[] { button4.Text, button5.Text, button6.Text },
+        new string[] { button7.Text, button8.Text, button9.Text },
+        new string[] { button1.Text, button4.Text, button7.Text },
+        new string[] { button2.Text, button5.Text, button8.Text },
+        new string[] { button3.Text, button6.Text, button9.Text },
+        new string[] { button1.Text, button5.Text, button9.Text },
+        new string[] { button3.Text, button5.Text, button7.Text }
+            };
+
+            foreach (var combination in winningCombinations)
+            {
+                if (combination[0] != "" &&
+                    combination[0] == combination[1] &&
+                    combination[1] == combination[2])
+                {
+                    DeclareWinner(combination[0]);
+                    return;
+                }
+            }
+        }
+
+
+        private void DeclareWinner(string player)
+        {
+            DialogResult result = MessageBox.Show(player + " Wins! Play again?", "Tic Tac Toe", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                ResetGame();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void ResetGame()
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button button)
+                {
+                    button.Text = "";
+                    button.Enabled = true;
+                }
+            }
+            isPlayerXTurn = true;
+        }
+    }
+}
